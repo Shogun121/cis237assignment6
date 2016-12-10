6155c1b62ed6da218039a1e3ab67aff5daee854b
Shogun121 <Daimyo121@aol.com>
2016-12-09 20:42:49 -0500

    1)Added menu nav item for 'Beverages'. 2)Only allow access to 'Beverages' if signed in. 3)Changed Beverage index ViewBag.title='Beverage List'. 4)Added the data search filter. 5)Added the filter method. 6)Edited the ActionResult Index method. 7)Added /[Authorize] to the top of the Beverage controller class. 8)Added /[HttpPost] and [ValidateAFToken] above Filter Method.

--- cis237assignment6/Controllers/BeveragesController.cs
+++ cis237assignment6/Controllers/BeveragesController.cs
@@ -10,6 +10,7 @@ using cis237assignment6.Models;
 
 namespace cis237assignment6.Controllers
 {
+    [Authorize]
     public class BeveragesController : Controller
     {
         private BeverageRCooleyEntities db = new BeverageRCooleyEntities();
@@ -17,7 +18,74 @@ namespace cis237assignment6.Controllers
         // GET: Beverages
         public ActionResult Index()
         {
-            return View(db.Beverages.ToList());
+            //Set up a variable to hold the Beverage data-set.
+            DbSet<Beverage> BeverageToFind =db.Beverages;
+
+            //Setup some strings that might hold data from the session.
+            //If the session is empty we can still use them as default values.
+            string filterId = "";
+            string filterName = "";
+            string filterPack = "";
+            string filterMin = "";
+            string filterMax = "";
+            string filterActive = "";
+
+            //Define a min and a max for price.
+            int min = 0;
+            int max = 100;
+
+            //Check to see if there is a variable in the session, if there is assign it to the
+            //variable we setup to hold the value.
+            if(Session["id"]!=null && !String.IsNullOrWhiteSpace((string)Session["id"]))
+            {
+                filterId = (string)Session["id"];
+            }
+            if(Session["name"]!=null && !String.IsNullOrWhiteSpace((string)Session["name"]))
+            {
+                filterName = (string)Session["name"];
+            }
+            if (Session["pack"] != null && !String.IsNullOrWhiteSpace((string)Session["pack"]))
+            {
+                filterName = (string)Session["pack"];
+            }
+            if (Session["min"] != null && !String.IsNullOrWhiteSpace((string)Session["min"]))
+            {
+                filterMin = (string)Session["min"];
+                min = Int32.Parse(filterMin);
+            }
+            if (Session["Max"] != null && !String.IsNullOrWhiteSpace((string)Session["Max"]))
+            {
+                filterMax = (string)Session["Max"];
+                min = Int32.Parse(filterMax);
+            }
+            if (Session["active"] != null && !String.IsNullOrWhiteSpace((string)Session["active"]))
+            {
+                filterName = (string)Session["active"];
+            }
+
+            //Do the filter on the Beverage Item set. Use the where that we used before.
+            //When using EF this time send it more lamda expressions to narrow it down.
+            //Since we set the values for each of the filter values we can count on this running with no 
+            //errors.
+            IEnumerable<Beverage> filtered = BeverageToFind.Where(Beverage => Beverage.price >= min &&
+                                                                  Beverage.price <= max &&
+                                                                  Beverage.id.Contains(filterId) &&
+                                                                  Beverage.name.Contains(filterName)&&
+                                                                  Beverage.pack.Contains(filterPack)&&
+                                                                  Beverage.active.Equals(filterActive));
+
+            //Place the string representation of the values in the session into the ViewBag
+            //so that they can be retrieved and displayed on the view.
+            ViewBag.filterMake = filterId;
+            ViewBag.filterDesc = filterName;
+            ViewBag.filterPack = filterPack;
+            ViewBag.filterMin = filterMin;
+            ViewBag.filterMax = filterMax;
+            ViewBag.filterActive = filterActive;
+
+            //Return the view with a filtered selection of the beverages.
+            return View(filtered);
+            //return View(db.Beverages.ToList());
         }
 
         // GET: Beverages/Details/5
@@ -122,6 +190,31 @@ namespace cis237assignment6.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Filter()
+        {
+            //Get the form data that was sent out of the request object.
+            //The key that is used as key to get the data matches the name property of the form control.
+            //For us this is the first parameter.
+            String id = Request.Form.Get("id");
+            String name = Request.Form.Get("name");
+            String pack=Request.Form.Get("pack");
+            String min = Request.Form.Get("min");
+            String max = Request.Form.Get("max");
+            String active = Request.Form.Get("active");
+
+            //Store the form data in the session so we can retrieve it later on the filter.
+            Session["id"] = "";
+            Session["name"] = "";
+            Session["pack"] = "";
+            Session["min"] = "";
+            Session["max"] = "";
+            Session["active"] = "";
+
+            //redirect the user to the index page we will do the work of filtering in the index method.
+            return RedirectToAction("Index");
         }
     }
 }
